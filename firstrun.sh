97cfc536da1aae122276fc1a54aef10bcc4b22d9
Steve Harris <git@steveharris.me>
2020-05-11 08:43:05 +0100

    added firstrun script

---
+++ firstrun.sh
@@ -0,0 +1,9 @@
+
+add-apt-repository ppa:martin-frost/thoughtbot-rcm
+add-apt-repository ppa:keithw/mosh
+apt-get update
+apt-get install mosh rcm neovim fzf ripgrep
+
+# symlinks
+ln -s /mnt/c/Users/steve/Onedrive $HOME
+
